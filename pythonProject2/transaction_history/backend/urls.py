612c45a3186511976910ea1246f22e168fb88480
ystas1205 <130070453+ystas1205@users.noreply.github.com>
2024-09-14 16:53:29 +0300

    Delete pythonProject2 directory

--- pythonProject2/transaction_history/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from backend.views import ImportProduct
-
-app_name = 'backend'
-urlpatterns = [
-    path('import', ImportProduct.as_view(), name='importproduct'),
-
-]
